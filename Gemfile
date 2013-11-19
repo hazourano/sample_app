695ecd22108c00b4c7b3b1107e82ba8d1b0c30c2
hazourano <hazouranosan@gmail.com>
2013-11-19 16:47:40 +0800

    Additional exercise and testing setup

--- Gemfile
+++ Gemfile
@@ -7,11 +7,15 @@ group :development, :test do
 # Use sqlite3 as the database for Active Record
 	gem 'sqlite3'
 	gem 'rspec-rails', '2.13.1'
+	gem 'guard-rspec', '2.5.0'
 end
 
 group :test do
 	gem 'selenium-webdriver', '2.35.1'
 	gem 'capybara', '2.1.0'
+	gem 'spork-rails', '4.0.0'
+	gem 'guard-spork', '1.5.0'
+	gem 'childprocess', '0.3.9'
 end
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -23,6 +23,7 @@ describe "Static pages" do
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
+
 	describe "About page" do
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
@@ -31,6 +32,17 @@ describe "Static pages" do
 		it "should have the right title" do
 			visit '/static_pages/about'
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+		end
+	end
+
+	describe "Contact page" do
+		it "should have the content 'Contact Us'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact Us')
+		end
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
 		end
 	end
 end
