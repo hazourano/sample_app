338412b9a24da619c410364a97031598d97c4370
hazourano <hazouranosan@gmail.com>
2013-11-21 14:49:07 +0800

    Additional Exercise

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,47 +1,35 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  	shared_examples_for "all static pages" do
+    	it { should have_selector('h1', text: heading) }
+    	it { should have_title(full_title(page_title)) }
+  	end
+
 	subject {page}
-	describe "Home page" do
-		before {visit root_path}
-		it "should have the content 'Sample App'" do
-			expect(page).to have_content('Sample App')
-		end
-		it "should have the base title" do
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-		end
-		it "should not have a custom page title" do
-			expect(page).not_to have_title('| Home')
-		end
-	end	
 
-	describe "Help page" do
-		before {visit help_path}
-		it "should have the content 'Help'" do
-			expect(page).to have_content('Help')
-		end
-		it "should have the right title" do
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-		end
-	end
+  	describe "Home page" do
+    	before { visit root_path }
+    	let(:heading)    { 'Sample App' }
+    	let(:page_title) { '' }
+    	it_should_behave_like "all static pages"
+    	it { should_not have_title('| Home') }
+  	end	
+  	describe "Help page" do
+    	before { visit root_path }
+    	let(:heading)    { 'Help' }
+    	let(:page_title) { 'Help' }
+  	end
+  	describe "About page" do
+    	before { visit root_path }
+    	let(:heading)    { 'About Us' }
+    	let(:page_title) { 'About Us' }
+  	end
 
-	describe "About page" do
-		before {visit about_path}
-		it "should have the content 'About Us'" do
-			expect(page).to have_content('About Us')
-		end
-		it "should have the right title" do
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-		end
-	end
-
-	describe "Contact page" do
-		before {visit contact_path}
-		it "should have the content 'Contact Us'" do
-			expect(page).to have_content('Contact Us')
-		end
-		it "should have the right title" do
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
-		end
-	end
+  	describe "Contact page" do
+    	before { visit contact_path }
+    	it { should have_selector('h1', text: 'Contact Us') }
+    	it { should have_title(full_title('Contact Us')) }
+  	end
 end
